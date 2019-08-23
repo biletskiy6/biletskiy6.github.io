bfafb253d2f7cd8a0981b8561b2d3d5685ce9b90
biletskiy6 <victorbiletskiy82@gmail.com>
2019-08-23 12:37:14 +0300

    star-db init

---
+++ star-db/precache-manifest.459adc8726949a2cac7f65b342340bfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689d5355a5f49de9e20013d7ead20c63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "426545d61d93727cf288",
+    "url": "/static/css/main.7d6cf392.chunk.css"
+  },
+  {
+    "revision": "215aa2aa8b736e62714c",
+    "url": "/static/js/2.1084bb38.chunk.js"
+  },
+  {
+    "revision": "426545d61d93727cf288",
+    "url": "/static/js/main.ef4466a1.chunk.js"
+  },
+  {
+    "revision": "faec94dabe86205847a3",
+    "url": "/static/js/runtime~main.fd2ad8a4.js"
+  }
+]);
